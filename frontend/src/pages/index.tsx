8e720b9cc851e2a0f5767fa81455884939d04345
MatievisTheKat <matievisthekat@gmail.com>
2020-09-12 13:50:11 +0200

    setup frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {}
+interface State {}
+
+export default class Index extends React.Component<Props, State> {
+  constructor(props: Readonly<Props>) {
+    super(props);
+  }
+
+  public render() {
+    return <></>;
+  }
+}
