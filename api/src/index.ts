6d4b58163813def12daabceb9b0bed706ad9699e
MatievisTheKat <matievisthekat@gmail.com>
2020-11-03 12:29:56 +0200

    chore: actually no need for a public path

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,24 +7,6 @@ import { Util } from "./util/Util";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const pubPath = resolve("public");
-
-fs.access(pubPath, (err) => {
-  if (err) {
-    Logger.warn(err);
-
-    fs.mkdir(pubPath, (err) => {
-      if (err) {
-        Logger.error(err);
-        Logger.warn(`Failed to create 'public' directory at ${pubPath}. There should be additional logging above`);
-      } else {
-        Logger.info(`Create 'public' directory at ${pubPath}`);
-      }
-    });
-  }
-});
-
-app.use(express.static(pubPath));
 
 const routeFiles = Util.findNested(join(__dirname, "routes"));
 for (const file of routeFiles) {
