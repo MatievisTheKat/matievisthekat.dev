63c3ac5e2ece62fcf584038c8fcc2f671a41a9bb
MatievisTheKat <matievisthekat@gmail.com>
2020-11-03 12:25:34 +0200

    chore: create public folder if not found

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import { join, resolve } from "path";
+import fs from "fs";
 import { Route } from "./types";
 import { Logger } from "./util/Logger";
 import { Util } from "./util/Util";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const pubPath = resolve("public");
 
-app.use(express.static(resolve("public")));
+fs.access(pubPath, (err) => {
+  if (err) {
+    Logger.warn(err);
+
+    fs.mkdir(pubPath, (err) => {
+      if (err) {
+        Logger.error(err);
+        Logger.warn(`Failed to create 'public' directory at ${pubPath}. There should be additional logging above`);
+      } else {
+        Logger.info(`Create 'public' directory at ${pubPath}`);
+      }
+    });
+  }
+});
+
+app.use(express.static(pubPath));
 
 const routeFiles = Util.findNested(join(__dirname, "routes"));
 for (const file of routeFiles) {
