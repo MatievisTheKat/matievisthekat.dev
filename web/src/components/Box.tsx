eab151efd54683587e10a3b5664e80cae34e483b
MatievisTheKat <matievisthekat@gmail.com>
2020-11-07 21:02:45 +0200

    feat(components): add Box component

---
+++ web/src/components/Box.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface State {}
+interface Props {
+  className?: string;
+}
+
+export default class Box extends React.Component<Props, State> {
+  constructor(props: Props | Readonly<Props>) {
+    super(props);
+  }
+
+  public render() {
+    return <div className={`w-full max-w-md mx-auto bg-gray-100 shadow-md rounded p-4 ${this.props.className}`}>{this.props.children}</div>;
+  }
+}
