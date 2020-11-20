7a34aa1ca23f8907c09e8a2619e05c02e5229979
MatievisTheKat <matievisthekat@gmail.com>
2020-11-20 12:20:02 +0200

    feat(avatars): add default avatar and /avatars endpoint

--- api/src/routes/avatars.ts
+++ api/src/routes/avatars.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import { Route } from "../types";
+import Response from "../util/Response";
+import fs from "fs-extra";
+import path from "path";
 
 const router = Router();
 
 router.get("/default/random", (req, res) => res.redirect(`/public/avatars/default/${Math.floor(Math.random() * 80)}.png`));
-router.get("/default/:id", (req, res) => res.redirect(`/public/avatars/default/${req.params.id}.png`));
+router.get("/default/:id", (req, res) => res.redirect(`/public/avatars/default/${req.params.id}${req.params.id.endsWith(".png") ? "" : ".png"}`));
+router.get("/default", (req, res) => res.redirect("/public/avatars/default.png"));
+
+router.get("/", async (req, res) => {
+  const fileNames = await fs.readdir(path.resolve("./public/avatars/default/"));
+  const files = fileNames.map((f) => `/avatars/default/${f}`);
+
+  new Response({
+    status: 200,
+    data: files,
+  }).send(res);
+});
 
 export const route: Route = {
   path: "/avatars",
