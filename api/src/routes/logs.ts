4e638e1ab4defe8095685cf35764323e28830c04
MatievisTheKat <matievisthekat@gmail.com>
2020-11-26 20:12:01 +0200

    feat(logging): add perma-logging

---
+++ api/src/routes/logs.ts
@@ -0,0 +1,58 @@
+import express, { Router } from "express";
+import { Route } from "../types";
+import fs from "fs-extra";
+import { resolve } from "path";
+import ApiResponse from "../util/Response";
+
+const router = Router();
+const logs = resolve("logs");
+const getLogs = async () => await fs.readdir(logs);
+
+fs.ensureDir(logs);
+
+router.use("/file", express.static(logs));
+
+router.get("/", async (req, res) => new ApiResponse({ status: 200, data: await getLogs() }).send(res));
+
+router.get("/:year", async (req, res) => {
+  const files = (await getLogs()).filter((f) => f.split("-")[0] === req.params.year);
+  if (!files) return new ApiResponse({ status: 404 }).send(res);
+
+  new ApiResponse({ status: 200, data: files }).send(res);
+});
+
+router.get("/:year/:month", async (req, res) => {
+  const files = (await getLogs()).filter((f) => {
+    const parts = f.split("-");
+    const year = parts[0];
+    const month = parts[1];
+
+    return year === req.params.year && month === req.params.month;
+  });
+
+  if (!files) return new ApiResponse({ status: 404 }).send(res);
+
+  new ApiResponse({ status: 200, data: files }).send(res);
+});
+
+router.get("/:year/:month/:day", async (req, res) => {
+  const { year, month, day } = req.params;
+
+  const file = (await getLogs()).find((f) => {
+    const parts = f.split("-");
+    const fileYear = parts[0];
+    const fileMonth = parts[1];
+    const fileDay = parts[2].split(".")[0];
+
+    return fileYear === year && fileMonth === month && fileDay === day;
+  });
+
+  if (!file) return new ApiResponse({ status: 404 }).send(res);
+
+  res.redirect(`/logs/file/${year}-${month}-${day}.log`);
+});
+
+export const route: Route = {
+  path: "/logs",
+  router,
+};
--- api/src/util/Logger.ts
+++ api/src/util/Logger.ts
@@ -2,6 +2,9 @@ import chalk from "chalk";
 import moment from "moment";
 import { NextFunction, Request, Response } from "express";
 import onFinished from "on-finished";
+import fs from "fs-extra";
+import { join, resolve } from "path";
+import { EventEmitter } from "events";
 
 const typeToColour: Record<string, string> = {
   log: chalk.bold("LOG"),
@@ -10,9 +13,25 @@ const typeToColour: Record<string, string> = {
   error: chalk.red("ERROR"),
 };
 
+export class LogStream extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  write(content: any) {
+    this.emit("write", content);
+  }
+
+  close() {
+    this.emit("close");
+  }
+}
+
+const writer = getLogStream();
+
 export class Logger {
   private static get date(): string {
-    return moment().format("DD/MM/YYYY hh:mm:ss a");
+    return moment().format("DD/MM/YYYY HH:mm:ss");
   }
 
   private static calcSpace(logType: string): string {
@@ -45,13 +64,40 @@ export class Logger {
 
     onFinished(res, (err, msg) => {
       const duration = Date.now() - startAt;
-      const status = res.statusCode;
+      const status = msg.statusCode;
 
       console.log(
         `${chalk.grey(Logger.date)}   ${chalk.cyan(req.method)}${Logger.calcSpace(req.method)}${duration}ms - ${chalk.yellow(status)} - ${path}`
       );
+
+      writer.write(`${Logger.date}  ${req.method}${Logger.calcSpace(req.method)}${duration}ms - ${status} - ${path}`);
     });
 
     next();
   }
+}
+
+function getLogStream(): LogStream {
+  const dir = resolve("logs");
+  const file = join(dir, `${moment().format("YYYY-MM-DD")}.log`);
+  const logStream = new LogStream();
+
+  (async () => {
+    const previousLogs = await fs.readFile(file).catch(() => {});
+    const stream = fs.createWriteStream(file);
+
+    if (previousLogs) {
+      stream.write(previousLogs);
+      stream.write("\n\n#<--- NEW PROCESS --->#\n\n\n");
+    }
+
+    logStream
+      .on("write", (content) => {
+        stream.write(content);
+        stream.write("\n");
+      })
+      .on("close", () => stream.close());
+  })();
+
+  return logStream;
 }
