54d3617f5b625b0704b8df6466d00153e693859a
MatievisTheKat <matievisthekat@gmail.com>
2020-12-13 15:40:34 +0200

    feat: add internet connectivity listener and notification popup

---
+++ web/src/components/layout/ConnectivityListener.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useToasts } from "react-toast-notifications";
+
+interface Props {}
+
+const ConnectivityListener: React.FC<Props> = (props) => {
+  const { addToast } = useToasts();
+  const [online, setOnline] = useState(window.navigator.onLine);
+  const [hasGoneOffline, setHasGoneOffline] = useState(false);
+
+  const onlineHandler = () => setOnline(true);
+  const offlineHandler = () => {
+    if (!hasGoneOffline) setHasGoneOffline(true);
+    setOnline(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", onlineHandler);
+    window.addEventListener("offline", offlineHandler);
+
+    return () => {
+      window.removeEventListener("online", onlineHandler);
+      window.removeEventListener("offline", offlineHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (hasGoneOffline)
+      addToast(
+        <div>
+          <strong>{online ? "Online" : "Offline"}</strong>
+          <div>{online ? "Internet connection is back!" : "Internet connection has been lost!"}</div>
+        </div>,
+        {
+          appearance: "info",
+        }
+      );
+  }, [online]);
+
+  return null;
+};
+
+export default ConnectivityListener;
