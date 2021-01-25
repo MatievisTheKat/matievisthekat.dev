3ead015007b2589458c419415a2177558f74bddf
MatievisTheKat <matievisthekat@gmail.com>
2021-01-25 10:41:13 +0200

    chore: clean up

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -5,18 +5,3 @@ exports.onPostBuild = async function () {
   const public = path.join(__dirname, "public");
   if (process.env.POST_BUILD_DIR) await fs.copy(public, process.env.POST_BUILD_DIR, { overwrite: true });
 };
-
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /peerjs/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  }
-};
