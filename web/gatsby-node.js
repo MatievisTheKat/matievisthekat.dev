dd625df264545f8b4345f9d59c23ef86105f521f
MatievisTheKat <matievisthekat@gmail.com>
2021-01-31 17:01:48 +0200

    clean up

--- web/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const fs = require("fs-extra");
-
-exports.onPostBuild = async function () {
-  const public = path.join(__dirname, "public");
-  if (process.env.POST_BUILD_DIR) await fs.copy(public, process.env.POST_BUILD_DIR, { overwrite: true });
-};
