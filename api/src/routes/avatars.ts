140b1212ee174524ee9687f7b0f41c7e20382d4d
MatievisTheKat <matievisthekat@gmail.com>
2021-02-08 18:17:15 +0200

    actual support for png avatars

--- api/src/routes/avatars.ts
+++ api/src/routes/avatars.ts
@@ -8,7 +8,7 @@ const router = Router();
 
 router.get("/default/random", (req, res) => res.redirect(`/public/avatars/default/${Math.floor(Math.random() * 80)}.webp`));
 router.get("/default/:id", (req, res) =>
-  res.redirect(`/public/avatars/default/${req.params.id}${req.params.id.endsWith(".webp") || req.params.id.endsWith(".png") ? "" : ".webp"}`)
+  res.redirect(`/public/avatars/default/${req.params.id.replace(/.png/g, "")}${req.params.id.endsWith(".webp") ? "" : ".webp"}`)
 );
 router.get("/default", (req, res) => res.redirect("/public/avatars/default.webp"));
 
